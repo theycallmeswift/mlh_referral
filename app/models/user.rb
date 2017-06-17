1e5fffa7361e5a0a6b741f7dd051d723ed10b76d
raf <raf@Rafaels-MacBook-Pro.local>
2017-06-16 23:28:12 -0400

    Add #generate_referral_code, #set_referral_code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :trackable
+
+  before_create :set_referral_code
+
+  private
+
+  def generate_referral_code
+    Digest::SHA1.hexdigest([ id, Time.now ].join)[0..5].upcase
+  end
+
+  def set_referral_code
+    self.referral_code = generate_referral_code
+  end
 end
